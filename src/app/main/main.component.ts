377d54d52b994029b66cd15ff1c041182d88e534
Jorge Loch <jorgembloch2001@gmail.com>
2024-04-28 00:09:55 -0300

    feat: adding about me section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [AboutMeComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
